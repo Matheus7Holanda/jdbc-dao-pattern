71a14bca6cde00d87cd99f9ea979b419e783c001
Matheus Holanda <matheusnh6@gmail.com>
2026-06-20 17:16:47 -0300

    DepartamentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Seller obj);
+    Department findbyId(Integer id);
+    List<Department> findAll();
+
+
+}
